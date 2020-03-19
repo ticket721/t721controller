f33c436add043b3096e597a03ce217ff5940cdb2
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-03-19 10:26:59 +0100

    fix: no more report merging

---
+++ .solcover.js
@@ -0,0 +1,11 @@
+module.exports = {
+    skipFiles: [
+        'v0/mocks/ERC20Mock_v0.sol',
+        'v0/mocks/DaiMock_v0.sol',
+        'v0/mocks/ITokenUriProvider.sol',
+        'v0/mocks/ERC721Mock_v0.sol',
+        'v0/ITicketForge_v0.sol',
+        'v0/SigUtil_v0.sol',
+        'v0/BytesUtil_v0.sol'
+    ]
+};
